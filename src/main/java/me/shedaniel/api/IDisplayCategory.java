418ee1e13fb66ef30c9473e0069695d89967124f
Unknown <shekwancheung0528@gmail.com>
2019-01-05 20:38:02 +0800

    Revert IDisplayCategory change

--- src/main/java/me/shedaniel/api/IDisplayCategory.java
+++ src/main/java/me/shedaniel/api/IDisplayCategory.java
@@ -2,8 +2,6 @@ package me.shedaniel.api;
 
 import me.shedaniel.gui.widget.Control;
 import me.shedaniel.gui.widget.REISlot;
-import net.minecraft.client.gui.Gui;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 import java.util.List;
@@ -11,31 +9,23 @@ import java.util.List;
 /**
  * Created by James on 8/7/2018.
  */
-public abstract class IDisplayCategory<T extends IRecipe> {
-    public abstract String getId();
+public interface IDisplayCategory<T extends IRecipe> {
+    public String getId();
     
-    public abstract String getDisplayName();
+    public String getDisplayName();
     
-    public abstract void addRecipe(T recipe);
+    public void addRecipe(T recipe);
     
-    public abstract void resetRecipes();
+    public void resetRecipes();
     
-    public abstract List<REISlot> setupDisplay(int number);
+    public List<REISlot> setupDisplay(int number);
     
-    public abstract boolean canDisplay(T recipe);
+    public boolean canDisplay(T recipe);
     
-    public abstract void drawExtras();
+    public void drawExtras();
     
-    public abstract void addWidget(List<Control> controls, int number);
+    public void addWidget(List<Control> controls, int number);
     
-    public abstract ItemStack getCategoryIcon();
-    
-    public boolean canAutoCraft(Class<? extends Gui> guiClass, T recipe) {
-        return false;
-    }
-    
-    public boolean performAutoCraft(Gui guiClass, T recipe) {
-    
-    }
+    public ItemStack getCategoryIcon();
     
 }
