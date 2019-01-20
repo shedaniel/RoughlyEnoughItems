b9b3c423d5610b7ff88d7c5b944219ae1bf5a663
Unknown <shekwancheung0528@gmail.com>
2019-01-20 20:30:48 +0800

    New Plugin Loader

---
+++ src/main/java/me/shedaniel/rei/api/REIPluginInfo.java
@@ -0,0 +1,34 @@
+package me.shedaniel.rei.api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class REIPluginInfo {
+    
+    public static Gson GSON = new GsonBuilder().create();
+    
+    private List<REIPlugin> plugins;
+    
+    public static class REIPlugin {
+        private String identifier;
+        @SerializedName("class") private String pluginClass;
+    
+        public String getIdentifier() {
+            if (identifier == null)
+                return "null:null";
+            return identifier;
+        }
+    
+        public String getPluginClass() {
+            return pluginClass;
+        }
+    }
+    
+    public List<REIPlugin> getPlugins() {
+        return plugins;
+    }
+    
+}
