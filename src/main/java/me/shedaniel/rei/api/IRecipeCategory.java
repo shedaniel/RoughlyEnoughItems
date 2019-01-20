e7046d4be84c818b8296d947a4d854819e15f3a7
Unknown <shekwancheung0528@gmail.com>
2019-01-20 16:45:39 +0800

    Cheating on servers

--- src/main/java/me/shedaniel/rei/api/IRecipeCategory.java
+++ src/main/java/me/shedaniel/rei/api/IRecipeCategory.java
@@ -3,17 +3,15 @@ package me.shedaniel.rei.api;
 import com.mojang.blaze3d.platform.GlStateManager;
 import me.shedaniel.rei.gui.widget.IWidget;
 import me.shedaniel.rei.gui.widget.RecipeBaseWidget;
+import me.shedaniel.rei.gui.widget.RecipeViewingWidget;
 import me.shedaniel.rei.listeners.IMixinContainerGui;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.Drawable;
-import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.GuiLighting;
-import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.render.VertexFormats;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -32,6 +30,15 @@ public interface IRecipeCategory<T extends IRecipeDisplay> {
     
     default public List<IWidget> setupDisplay(IMixinContainerGui containerGui, T recipeDisplay, Rectangle bounds) {
         return Arrays.asList(new RecipeBaseWidget(bounds));
+    }
+    
+    default public void drawCategoryBackground(Rectangle bounds) {
+        GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        GuiLighting.disable();
+        MinecraftClient.getInstance().getTextureManager().bindTexture(RecipeViewingWidget.CHEST_GUI_TEXTURE);
+        new Drawable() {
+        
+        }.drawTexturedRect((int) bounds.getX(), (int) bounds.getY(), 0, 0, (int) bounds.getWidth(), (int) bounds.getHeight());
     }
     
 }
