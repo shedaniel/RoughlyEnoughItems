4b8cf1ac0bf7b4c26f6dd68a10011d6c1a29fcf2
Unknown <shekwancheung0528@gmail.com>
2019-02-19 00:22:57 +0800

    ahh typos

--- src/main/java/me/shedaniel/rei/client/KeyBindHelper.java
+++ src/main/java/me/shedaniel/rei/client/KeyBindHelper.java
@@ -4,9 +4,7 @@ import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.client.util.InputMappings;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.client.settings.KeyModifier;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class KeyBindHelper {
     
@@ -15,8 +13,7 @@ public class KeyBindHelper {
     private static final String HIDE_KEYBIND = "key.roughlyenoughitems.hide_keybind";
     public static KeyBinding RECIPE, USAGE, HIDE;
     
-    @SubscribeEvent
-    public static void setupKeyBinds(FMLClientSetupEvent event) {
+    public static void setupKeyBinds() {
         String category = "key.rei.category";
         ClientRegistry.registerKeyBinding(RECIPE = new KeyBinding(RECIPE_KEYBIND, KeyConflictContext.IN_GAME, KeyModifier.NONE, InputMappings.Type.KEYSYM.getOrMakeInput(82), category));
         ClientRegistry.registerKeyBinding(USAGE = new KeyBinding(USAGE_KEYBIND, KeyConflictContext.IN_GAME, KeyModifier.NONE, InputMappings.Type.KEYSYM.getOrMakeInput(85), category));
