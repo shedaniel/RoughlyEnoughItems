2a5ebf385a00b4a0e1eab0ec23993e4697a0631b
Unknown <shekwancheung0528@gmail.com>
2019-05-11 14:35:39 +0800

    Build 107

--- src/main/java/me/shedaniel/rei/listeners/ContainerScreenHooks.java
+++ src/main/java/me/shedaniel/rei/listeners/ContainerScreenHooks.java
@@ -1,17 +1,26 @@
 package me.shedaniel.rei.listeners;
 
+import net.minecraft.client.gui.ContainerScreen;
 import net.minecraft.container.Slot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 
+@Mixin(ContainerScreen.class)
 public interface ContainerScreenHooks {
     
+    @Accessor("left")
     int rei_getContainerLeft();
     
+    @Accessor("top")
     int rei_getContainerTop();
     
+    @Accessor("containerWidth")
     int rei_getContainerWidth();
     
+    @Accessor("containerHeight")
     int rei_getContainerHeight();
     
+    @Accessor("focusedSlot")
     Slot rei_getHoveredSlot();
     
 }
