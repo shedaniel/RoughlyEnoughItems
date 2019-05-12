eab120d5619f972e29afc9776959f6d6096d3926
Unknown <shekwancheung0528@gmail.com>
2019-05-12 10:36:42 +0800

    More docs

--- src/main/java/me/shedaniel/rei/api/ItemRegistry.java
+++ src/main/java/me/shedaniel/rei/api/ItemRegistry.java
@@ -12,12 +12,29 @@ import java.util.List;
 
 public interface ItemRegistry {
     
+    /**
+     * Gets the current unmodifiable item list
+     *
+     * @return an unmodifiable item list
+     */
     List<ItemStack> getItemList();
     
+    /**
+     * Gets the current modifiable item list
+     *
+     * @return an modifiable item list
+     */
+    @Deprecated
     List<ItemStack> getModifiableItemList();
     
     ItemStack[] getAllStacksFromItem(Item item);
     
+    /**
+     * Registers an new stack to the item list
+     *
+     * @param afterItem
+     * @param stack     the stack to register
+     */
     void registerItemStack(Item afterItem, ItemStack stack);
     
     default void registerItemStack(Item afterItem, ItemStack... stacks) {
@@ -32,6 +49,12 @@ public interface ItemRegistry {
                 registerItemStack(null, stack);
     }
     
+    /**
+     * Checks if a stack is already registered
+     *
+     * @param stack the stack to check
+     * @return whether the stack has been registered
+     */
     default boolean alreadyContain(ItemStack stack) {
         return getItemList().stream().anyMatch(stack1 -> ItemStack.areEqual(stack, stack1));
     }
