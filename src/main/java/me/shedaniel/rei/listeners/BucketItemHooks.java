2247908470423ca82016ab79a69c974e76fc7580
shedaniel <daniel@shedaniel.me>
2020-03-05 15:49:06 +0800

    get fluid bucket
    Signed-off-by: shedaniel <daniel@shedaniel.me>

---
+++ src/main/java/me/shedaniel/rei/listeners/BucketItemHooks.java
@@ -0,0 +1,12 @@
+package me.shedaniel.rei.listeners;
+
+import net.minecraft.fluid.Fluid;
+import net.minecraft.item.BucketItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(BucketItem.class)
+public interface BucketItemHooks {
+    @Accessor("fluid")
+    Fluid getFluid();
+}
