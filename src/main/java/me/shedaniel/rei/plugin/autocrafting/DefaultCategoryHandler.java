f50acefb5712a3d838ade2f89520282cb0bc4fa9
shedaniel <daniel@shedaniel.me>
2020-08-08 18:12:31 +0800

    Improve auto crafting, now able to detect things on the grid

--- src/main/java/me/shedaniel/rei/plugin/autocrafting/DefaultCategoryHandler.java
+++ src/main/java/me/shedaniel/rei/plugin/autocrafting/DefaultCategoryHandler.java
@@ -32,6 +32,7 @@ import me.shedaniel.rei.api.EntryStack;
 import me.shedaniel.rei.api.TransferRecipeDisplay;
 import me.shedaniel.rei.server.ContainerInfo;
 import me.shedaniel.rei.server.ContainerInfoHandler;
+import me.shedaniel.rei.server.RecipeFinder;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
@@ -44,6 +45,7 @@ import net.minecraft.container.Container;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.registry.Registry;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -69,7 +71,7 @@ public class DefaultCategoryHandler implements AutoTransferHandler {
         if (recipe.getHeight() > containerInfo.getCraftingHeight(container) || recipe.getWidth() > containerInfo.getCraftingWidth(container))
             return Result.createFailed(I18n.translate("error.rei.transfer.too_small", containerInfo.getCraftingWidth(container), containerInfo.getCraftingHeight(container)));
         List<List<EntryStack>> input = recipe.getOrganisedInputEntries(containerInfo, container);
-        IntList intList = hasItems(input);
+        IntList intList = hasItems(container, containerInfo, input);
         if (!intList.isEmpty())
             return Result.createFailed("error.rei.not.enough.materials", intList);
         if (!canUseMovePackets())
@@ -103,11 +105,12 @@ public class DefaultCategoryHandler implements AutoTransferHandler {
         return -10;
     }
     
-    public IntList hasItems(List<List<EntryStack>> inputs) {
+    public IntList hasItems(Container container, ContainerInfo<Container> containerInfo, List<List<EntryStack>> inputs) {
         // Create a clone of player's inventory, and count
-        DefaultedList<ItemStack> copyMain = DefaultedList.of();
+        RecipeFinder recipeFinder = new RecipeFinder();
+        containerInfo.populateRecipeFinder(container, recipeFinder);
         for (ItemStack stack : MinecraftClient.getInstance().player.inventory.main) {
-            copyMain.add(stack.copy());
+            recipeFinder.addNormalItem(stack.copy());
         }
         IntList intList = new IntArrayList();
         for (int i = 0; i < inputs.size(); i++) {
@@ -115,19 +118,17 @@ public class DefaultCategoryHandler implements AutoTransferHandler {
             boolean done = possibleStacks.isEmpty();
             for (EntryStack possibleStack : possibleStacks) {
                 if (!done) {
-                    int invRequiredCount = possibleStack.getAmount();
-                    for (ItemStack stack : copyMain) {
-                        EntryStack entryStack = EntryStack.create(stack);
-                        if (entryStack.equals(possibleStack)) {
-                            while (invRequiredCount > 0 && !stack.isEmpty()) {
-                                invRequiredCount--;
-                                stack.decrement(1);
-                            }
+                    if (possibleStack.getType() == EntryStack.Type.ITEM) {
+                        int invRequiredCount = possibleStack.getAmount();
+                        int key = Registry.ITEM.getRawId(possibleStack.getItem());
+                        while (invRequiredCount > 0 && recipeFinder.contains(key)) {
+                            invRequiredCount--;
+                            recipeFinder.take(key, 1);
                         }
-                    }
-                    if (invRequiredCount <= 0) {
-                        done = true;
-                        break;
+                        if (invRequiredCount <= 0) {
+                            done = true;
+                            break;
+                        }
                     }
                 }
             }
