b83979b33a6b482341c96d00b5423a943f9cccb2
shedaniel <daniel@shedaniel.me>
2021-07-09 01:02:36 +0800

    Prevent error spam when the client joins server without recipe sync packets

--- runtime/src/main/java/me/shedaniel/rei/impl/client/ClientHelperImpl.java
+++ runtime/src/main/java/me/shedaniel/rei/impl/client/ClientHelperImpl.java
@@ -232,6 +232,11 @@ public class ClientHelperImpl implements ClientHelper {
     
     @ApiStatus.Internal
     public LongSet _getInventoryItemsTypes() {
+        try {
+            VanillaEntryTypes.ITEM.getDefinition();
+        } catch (NullPointerException e) {
+            return new LongOpenHashSet();
+        }
         return Minecraft.getInstance().player.getInventory().compartments.stream()
                 .flatMap(Collection::stream)
                 .map(EntryStacks::of)
