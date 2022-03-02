523550c7b255ec92a398a58b6051ca47b20049a4
shedaniel <daniel@shedaniel.me>
2022-03-02 12:47:17 +0800

    Remove config button tooltip

---
+++ runtime/src/main/java/me/shedaniel/rei/impl/client/gui/modules/entries/TextMenuEntry.java
@@ -0,0 +1,70 @@
+/*
+ * This file is licensed under the MIT License, part of Roughly Enough Items.
+ * Copyright (c) 2018, 2019, 2020, 2021, 2022 shedaniel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.shedaniel.rei.impl.client.gui.modules.entries;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import me.shedaniel.rei.impl.client.gui.modules.AbstractMenuEntry;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.network.chat.Component;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class TextMenuEntry extends AbstractMenuEntry {
+    public final Supplier<Component> text;
+    public Component lastText;
+    public int lastTextWidth;
+    
+    public TextMenuEntry(Supplier<Component> text) {
+        this.text = text;
+        this.lastText = text.get();
+        this.lastTextWidth = Math.max(0, font.width(lastText));
+    }
+    
+    private int getTextWidth() {
+        return lastTextWidth;
+    }
+    
+    @Override
+    public int getEntryWidth() {
+        return getTextWidth() + 4;
+    }
+    
+    @Override
+    public int getEntryHeight() {
+        return 12;
+    }
+    
+    @Override
+    public List<? extends GuiEventListener> children() {
+        return Collections.emptyList();
+    }
+    
+    @Override
+    public void render(PoseStack matrices, int mouseX, int mouseY, float delta) {
+        font.draw(matrices, lastText = text.get(), getX() + 2, getY() + 2, 8947848);
+        this.lastTextWidth = Math.max(0, font.width(lastText));
+    }
+}
