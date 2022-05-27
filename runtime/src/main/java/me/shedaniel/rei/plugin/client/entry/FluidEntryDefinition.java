be40970223e29232b8e7b0e8c7fa1ba8ac5262a2
shedaniel <daniel@shedaniel.me>
2022-05-27 20:30:37 +0800

    Disable fluid amount display on JEI slots

--- runtime/src/main/java/me/shedaniel/rei/plugin/client/entry/FluidEntryDefinition.java
+++ runtime/src/main/java/me/shedaniel/rei/plugin/client/entry/FluidEntryDefinition.java
@@ -289,7 +289,7 @@ public class FluidEntryDefinition implements EntryDefinition<FluidStack>, EntryS
                 return null;
             List<Component> toolTip = Lists.newArrayList(entry.asFormattedText());
             long amount = entry.getValue().getAmount();
-            if (amount >= 0) {
+            if (amount >= 0 && entry.get(EntryStack.Settings.FLUID_AMOUNT_VISIBLE)) {
                 String amountTooltip = I18n.get(FLUID_AMOUNT, entry.getValue().getAmount());
                 if (amountTooltip != null) {
                     toolTip.addAll(Stream.of(amountTooltip.split("\n")).map(TextComponent::new).collect(Collectors.toList()));
