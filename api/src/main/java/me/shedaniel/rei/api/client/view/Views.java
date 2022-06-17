020a705d3947c38c1f27d6d322bade09a17c0853
shedaniel <daniel@shedaniel.me>
2022-06-17 21:52:16 +0800

    Too much docs for today

--- api/src/main/java/me/shedaniel/rei/api/client/view/Views.java
+++ api/src/main/java/me/shedaniel/rei/api/client/view/Views.java
@@ -31,6 +31,9 @@ import me.shedaniel.rei.api.common.registry.Reloadable;
 import java.util.Collection;
 
 public interface Views extends Reloadable<REIClientPlugin> {
+    /**
+     * @return the instance of {@link Views}
+     */
     static Views getInstance() {
         return PluginManager.getClientInstance().get(Views.class);
     }
