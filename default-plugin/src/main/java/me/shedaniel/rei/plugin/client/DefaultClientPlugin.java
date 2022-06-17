17a06b67547a6760011f97a557c8bba0198063dc
shedaniel <daniel@shedaniel.me>
2022-06-17 17:52:20 +0800

    Make gamemode favorites not pinned by default

--- default-plugin/src/main/java/me/shedaniel/rei/plugin/client/DefaultClientPlugin.java
+++ default-plugin/src/main/java/me/shedaniel/rei/plugin/client/DefaultClientPlugin.java
@@ -349,7 +349,7 @@ public class DefaultClientPlugin implements REIClientPlugin, BuiltinClientPlugin
     public void registerFavorites(FavoriteEntryType.Registry registry) {
         registry.register(GameModeFavoriteEntry.ID, GameModeFavoriteEntry.Type.INSTANCE);
         registry.getOrCrateSection(Component.translatable(GameModeFavoriteEntry.TRANSLATION_KEY))
-                .add(true, Stream.concat(
+                .add(Stream.concat(
                         Arrays.stream(GameType.values())
                                 .filter(type -> type.getId() >= 0),
                         Stream.of((GameType) null)
