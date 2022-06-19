8d4e35e2982b1ee1598564a41cf02e38729c0d1b
shedaniel <daniel@shedaniel.me>
2022-06-20 00:33:48 +0800

    Implement Config Addons, Close #908

---
+++ runtime/src/main/java/me/shedaniel/rei/impl/client/config/addon/ConfigAddonsScreen.java
@@ -0,0 +1,193 @@
+/*
+ * This file is licensed under the MIT License, part of Roughly Enough Items.
+ * Copyright (c) 2018, 2019, 2020, 2021, 2022 shedaniel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.shedaniel.rei.impl.client.config.addon;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import me.shedaniel.clothconfig2.gui.widget.DynamicElementListWidget;
+import me.shedaniel.rei.api.client.config.addon.ConfigAddon;
+import me.shedaniel.rei.api.client.config.addon.ConfigAddonRegistry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.narration.NarratableEntry;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.locale.Language;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.FormattedText;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import static me.shedaniel.rei.impl.client.gui.screen.DefaultDisplayViewingScreen.CHEST_GUI_TEXTURE;
+
+public class ConfigAddonsScreen extends Screen {
+    private AddonsList rulesList;
+    private final Screen parent;
+    
+    public ConfigAddonsScreen(Screen parent) {
+        super(new TranslatableComponent("text.rei.addons"));
+        this.parent = parent;
+    }
+    
+    @Override
+    public void init() {
+        super.init();
+        {
+            Component backText = new TextComponent("↩ ").append(new TranslatableComponent("gui.back"));
+            addRenderableWidget(new Button(4, 4, Minecraft.getInstance().font.width(backText) + 10, 20, backText, button -> {
+                minecraft.setScreen(parent);
+            }));
+        }
+        rulesList = addWidget(new AddonsList(minecraft, width, height, 30, height, BACKGROUND_LOCATION));
+        ConfigAddonRegistryImpl addonRegistry = (ConfigAddonRegistryImpl) ConfigAddonRegistry.getInstance();
+        for (ConfigAddon addon : addonRegistry.getAddons()) {
+            rulesList.addItem(new DefaultAddonEntry(parent, addon));
+        }
+    }
+    
+    @Override
+    public void render(PoseStack matrices, int mouseX, int mouseY, float delta) {
+        this.rulesList.render(matrices, mouseX, mouseY, delta);
+        super.render(matrices, mouseX, mouseY, delta);
+        this.font.drawShadow(matrices, this.title.getVisualOrderText(), this.width / 2.0F - this.font.width(this.title) / 2.0F, 12.0F, -1);
+    }
+    
+    public static class AddonsList extends DynamicElementListWidget<AddonEntry> {
+        private boolean inFocus;
+        
+        public AddonsList(Minecraft client, int width, int height, int top, int bottom, ResourceLocation backgroundLocation) {
+            super(client, width, height, top, bottom, backgroundLocation);
+        }
+        
+        @Override
+        public boolean changeFocus(boolean lookForwards) {
+            if (!this.inFocus && this.getItemCount() == 0) {
+                return false;
+            } else {
+                this.inFocus = !this.inFocus;
+                if (this.inFocus && this.getSelectedItem() == null && this.getItemCount() > 0) {
+                    this.moveSelection(1);
+                } else if (this.inFocus && this.getSelectedItem() != null) {
+                    this.getSelectedItem();
+                }
+                
+                return this.inFocus;
+            }
+        }
+        
+        @Override
+        protected boolean isSelected(int index) {
+            return false;
+        }
+        
+        @Override
+        protected int addItem(AddonEntry item) {
+            return super.addItem(item);
+        }
+        
+        @Override
+        public int getItemWidth() {
+            return width - 40;
+        }
+        
+        @Override
+        protected int getScrollbarPosition() {
+            return width - 14;
+        }
+    }
+    
+    public static abstract class AddonEntry extends DynamicElementListWidget.ElementEntry<AddonEntry> {
+        @Override
+        public int getItemHeight() {
+            return 26;
+        }
+        
+        @Override
+        public boolean changeFocus(boolean lookForwards) {
+            return false;
+        }
+    }
+    
+    public static class DefaultAddonEntry extends AddonEntry {
+        private final Button configureButton;
+        private final ConfigAddon addon;
+        
+        public DefaultAddonEntry(Screen parent, ConfigAddon addon) {
+            this.addon = addon;
+            this.configureButton = new Button(0, 0, 20, 20, Component.nullToEmpty(null), button -> {
+                Minecraft.getInstance().setScreen(this.addon.createScreen(Minecraft.getInstance().screen));
+            }) {
+                @Override
+                protected void renderBg(PoseStack matrices, Minecraft client, int mouseX, int mouseY) {
+                    super.renderBg(matrices, client, mouseX, mouseY);
+                    RenderSystem.setShaderTexture(0, CHEST_GUI_TEXTURE);
+                    blit(matrices, x + 3, y + 3, 0, 0, 14, 14);
+                }
+            };
+        }
+        
+        @Override
+        public void render(PoseStack matrices, int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean isHovered, float delta) {
+            Minecraft client = Minecraft.getInstance();
+            {
+                Component title = addon.getName();
+                int i = client.font.width(title);
+                if (i > entryWidth - 28) {
+                    FormattedText titleTrimmed = FormattedText.composite(client.font.substrByWidth(title, entryWidth - 28 - client.font.width("...")), FormattedText.of("..."));
+                    client.font.drawShadow(matrices, Language.getInstance().getVisualOrder(titleTrimmed), x + 2, y + 1, 16777215);
+                } else {
+                    client.font.drawShadow(matrices, title.getVisualOrderText(), x + 2, y + 1, 16777215);
+                }
+            }
+            {
+                Component subtitle = addon.getDescription();
+                int i = client.font.width(subtitle);
+                if (i > entryWidth - 28) {
+                    FormattedText subtitleTrimmed = FormattedText.composite(client.font.substrByWidth(subtitle, entryWidth - 28 - client.font.width("...")), FormattedText.of("..."));
+                    client.font.drawShadow(matrices, Language.getInstance().getVisualOrder(subtitleTrimmed), x + 2, y + 12, 8421504);
+                } else {
+                    client.font.drawShadow(matrices, subtitle.getVisualOrderText(), x + 2, y + 12, 8421504);
+                }
+            }
+            configureButton.x = x + entryWidth - 25;
+            configureButton.y = y + 1;
+            configureButton.render(matrices, mouseX, mouseY, delta);
+        }
+        
+        @Override
+        public List<? extends GuiEventListener> children() {
+            return Collections.singletonList(configureButton);
+        }
+        
+        @Override
+        public List<? extends NarratableEntry> narratables() {
+            return Collections.singletonList(configureButton);
+        }
+    }
+}
