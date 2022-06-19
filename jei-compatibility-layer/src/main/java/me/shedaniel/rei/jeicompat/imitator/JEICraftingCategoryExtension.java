29714219f1861325ef58db07eefae9880f7862c1
shedaniel <daniel@shedaniel.me>
2022-06-19 20:05:09 +0800

    Fix #875

---
+++ jei-compatibility-layer/src/main/java/me/shedaniel/rei/jeicompat/imitator/JEICraftingCategoryExtension.java
@@ -0,0 +1,97 @@
+/*
+ * This file is licensed under the MIT License, part of Roughly Enough Items.
+ * Copyright (c) 2018, 2019, 2020, 2021, 2022 shedaniel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.shedaniel.rei.jeicompat.imitator;
+
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.ingredient.ICraftingGridHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.category.extensions.vanilla.crafting.ICraftingCategoryExtension;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.CraftingRecipe;
+import net.minecraftforge.common.crafting.IShapedRecipe;
+import net.minecraftforge.common.util.Size2i;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+/**
+ * The following method is licensed with The MIT License (MIT)
+ * Copyright (c) 2014-2015 mezz
+ * <p>
+ * Full license text can be found in the https://github.com/mezz/JustEnoughItems/blob/1.17/LICENSE.txt
+ */
+public class JEICraftingCategoryExtension<T extends CraftingRecipe> implements ICraftingCategoryExtension {
+    protected final T recipe;
+    
+    public JEICraftingCategoryExtension(T recipe) {
+        this.recipe = recipe;
+    }
+    
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, ICraftingGridHelper craftingGridHelper, IFocusGroup focuses) {
+        List<List<ItemStack>> inputs = recipe.getIngredients().stream()
+                .map(ingredient -> List.of(ingredient.getItems()))
+                .toList();
+        ItemStack resultItem = recipe.getResultItem();
+        
+        int width = getWidth();
+        int height = getHeight();
+        craftingGridHelper.setOutputs(builder, VanillaTypes.ITEM_STACK, List.of(resultItem));
+        craftingGridHelper.setInputs(builder, VanillaTypes.ITEM_STACK, inputs, width, height);
+    }
+    
+    @Nullable
+    @Override
+    public ResourceLocation getRegistryName() {
+        return recipe.getId();
+    }
+    
+    @SuppressWarnings("removal")
+    @Nullable
+    @Override
+    public Size2i getSize() {
+        if (recipe instanceof IShapedRecipe<?> shapedRecipe) {
+            return new Size2i(shapedRecipe.getRecipeWidth(), shapedRecipe.getRecipeHeight());
+        }
+        return null;
+    }
+    
+    @Override
+    public int getWidth() {
+        if (recipe instanceof IShapedRecipe<?> shapedRecipe) {
+            return shapedRecipe.getRecipeWidth();
+        }
+        return 0;
+    }
+    
+    @Override
+    public int getHeight() {
+        if (recipe instanceof IShapedRecipe<?> shapedRecipe) {
+            return shapedRecipe.getRecipeHeight();
+        }
+        return 0;
+    }
+}
