025fc7576e962e428b77989f04f0913d17764fd2
shedaniel <daniel@shedaniel.me>
2022-06-20 00:33:48 +0800

    Implement Config Addons, Close #908

--- runtime/src/main/java/me/shedaniel/rei/RoughlyEnoughItemsCoreClient.java
+++ runtime/src/main/java/me/shedaniel/rei/RoughlyEnoughItemsCoreClient.java
@@ -60,6 +60,7 @@ import me.shedaniel.rei.api.common.util.ImmutableTextComponent;
 import me.shedaniel.rei.impl.ClientInternals;
 import me.shedaniel.rei.impl.client.REIRuntimeImpl;
 import me.shedaniel.rei.impl.client.config.ConfigManagerImpl;
+import me.shedaniel.rei.impl.client.config.addon.ConfigAddonRegistryImpl;
 import me.shedaniel.rei.impl.client.entry.renderer.EntryRendererRegistryImpl;
 import me.shedaniel.rei.impl.client.favorites.DelegatingFavoriteEntryProviderImpl;
 import me.shedaniel.rei.impl.client.favorites.FavoriteEntryTypeRegistryImpl;
@@ -234,7 +235,8 @@ public class RoughlyEnoughItemsCoreClient {
                 new FavoriteEntryTypeRegistryImpl(),
                 new SubsetsRegistryImpl(),
                 new TransferHandlerRegistryImpl(),
-                new REIRuntimeImpl()), "clientPluginManager");
+                new REIRuntimeImpl(),
+                new ConfigAddonRegistryImpl()), "clientPluginManager");
     }
     
     public void onInitializeClient() {
