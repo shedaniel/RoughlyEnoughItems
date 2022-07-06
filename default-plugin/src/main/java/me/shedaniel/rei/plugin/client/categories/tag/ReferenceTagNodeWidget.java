0b4f779e7ad499457016b528429177f0ddda1800
shedaniel <daniel@shedaniel.me>
2022-07-06 17:34:35 +0800

    Fix #963

--- default-plugin/src/main/java/me/shedaniel/rei/plugin/client/categories/tag/ReferenceTagNodeWidget.java
+++ default-plugin/src/main/java/me/shedaniel/rei/plugin/client/categories/tag/ReferenceTagNodeWidget.java
@@ -29,6 +29,7 @@ import me.shedaniel.math.Rectangle;
 import me.shedaniel.rei.api.client.gui.widgets.Slot;
 import me.shedaniel.rei.api.client.gui.widgets.Tooltip;
 import me.shedaniel.rei.api.client.gui.widgets.Widgets;
+import me.shedaniel.rei.api.client.util.MatrixUtils;
 import me.shedaniel.rei.api.common.entry.EntryStack;
 import me.shedaniel.rei.api.common.util.EntryIngredients;
 import me.shedaniel.rei.plugin.common.displays.tag.TagNode;
@@ -44,12 +45,14 @@ import java.util.function.Function;
 
 public class ReferenceTagNodeWidget<S, T> extends TagNodeWidget<S, T> {
     private final TagNode<S> node;
+    private final Rectangle overflowBounds;
     private final Rectangle bounds;
     private final Slot slot;
     private final List<? extends GuiEventListener> children;
     
-    public ReferenceTagNodeWidget(TagNode<S> node, Function<Holder<S>, EntryStack<T>> mapper) {
+    public ReferenceTagNodeWidget(TagNode<S> node, Function<Holder<S>, EntryStack<T>> mapper, Rectangle overflowBounds) {
         this.node = node;
+        this.overflowBounds = overflowBounds;
         this.bounds = new Rectangle(0, 0, 24, 23);
         this.slot = Widgets.createSlot(new Rectangle(0, 0, 18, 18))
                 .disableBackground()
@@ -66,13 +69,15 @@ public class ReferenceTagNodeWidget<S, T> extends TagNodeWidget<S, T> {
     
     @Override
     public void render(PoseStack poses, int mouseX, int mouseY, float delta) {
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderTexture(0, new ResourceLocation("textures/gui/advancements/widgets.png"));
-        this.blit(poses, bounds.x, bounds.y, 1, 128 + 27, 24, 24);
-        this.slot.getBounds().setLocation(bounds.getCenterX() - this.slot.getBounds().getWidth() / 2, bounds.y + (bounds.height - this.slot.getBounds().getHeight()) / 2 + 1);
-        this.slot.render(poses, mouseX, mouseY, delta);
-        if (this.containsMouse(mouseX, mouseY)) {
+        if (this.overflowBounds.intersects(MatrixUtils.transform(poses.last().pose(), getBounds()))) {
+            RenderSystem.setShader(GameRenderer::getPositionTexShader);
+            RenderSystem.setShaderTexture(0, new ResourceLocation("textures/gui/advancements/widgets.png"));
+            this.blit(poses, bounds.x, bounds.y, 1, 128 + 27, 24, 24);
+            this.slot.getBounds().setLocation(bounds.getCenterX() - this.slot.getBounds().getWidth() / 2, bounds.y + (bounds.height - this.slot.getBounds().getHeight()) / 2 + 1);
+            this.slot.render(poses, mouseX, mouseY, delta);
+            if (this.containsMouse(mouseX, mouseY)) {
             Tooltip.create(Component.literal("#" + this.node.getReference().location().toString())).queue();
+            }
         }
     }
     
