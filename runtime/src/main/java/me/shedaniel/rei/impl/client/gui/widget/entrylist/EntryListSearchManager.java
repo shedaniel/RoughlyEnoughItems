4ceefb00f0f452a3b14bb39e4183f7126648b051
shedaniel <daniel@shedaniel.me>
2022-12-25 00:11:01 +0800

    Stop searching when EntryRegistry is reloading

--- runtime/src/main/java/me/shedaniel/rei/impl/client/gui/widget/entrylist/EntryListSearchManager.java
+++ runtime/src/main/java/me/shedaniel/rei/impl/client/gui/widget/entrylist/EntryListSearchManager.java
@@ -80,6 +80,7 @@ public class EntryListSearchManager {
     }, HashedEntryStackWrapper::normalize);
     
     private static List<EntryStack<?>> getAllEntriesContextually(SearchFilter filter) {
+        if (EntryRegistry.getInstance().isReloading()) return List.of();
         if (ConfigObject.getInstance().isHidingEntryPanelIfIdle() && filter.getFilter().isEmpty()) {
             return List.of();
         }
