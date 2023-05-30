ac60edc6ec2f55954aa1b86e162a80607fce23fa
shedaniel <daniel@shedaniel.me>
2023-05-30 12:32:54 +0800

    Fix #1398

---
+++ fabric/src/main/java/me/shedaniel/rei/mixin/fabric/MixinInputConstantsKey.java
@@ -0,0 +1,58 @@
+/*
+ * This file is licensed under the MIT License, part of Roughly Enough Items.
+ * Copyright (c) 2018, 2019, 2020, 2021, 2022, 2023 shedaniel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.shedaniel.rei.mixin.fabric;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import com.mojang.blaze3d.systems.RenderSystem;
+import me.shedaniel.rei.api.client.config.ConfigObject;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(InputConstants.Key.class)
+public abstract class MixinInputConstantsKey {
+    @Shadow
+    public abstract String getName();
+    
+    @Inject(method = "getDisplayName", at = @At("HEAD"), cancellable = true)
+    private void getDisplayName(CallbackInfoReturnable<Component> cir) {
+        if (isPatchingAsyncThreadCrash() && !RenderSystem.isOnRenderThread()) {
+            cir.setReturnValue(new TranslatableComponent(getName()));
+        }
+    }
+    
+    @Unique
+    private static boolean isPatchingAsyncThreadCrash() {
+        try {
+            return ConfigObject.getInstance().isPatchingAsyncThreadCrash();
+        } catch (Throwable throwable) {
+            return false;
+        }
+    }
+}
