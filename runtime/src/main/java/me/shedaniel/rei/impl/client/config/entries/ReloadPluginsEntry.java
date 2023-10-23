032a0ff4989fdb276e0b4c8b6b15bb60ac723bff
shedaniel <daniel@shedaniel.me>
2023-10-23 21:45:09 +0800

    Close #1505

--- runtime/src/main/java/me/shedaniel/rei/impl/client/config/entries/ReloadPluginsEntry.java
+++ runtime/src/main/java/me/shedaniel/rei/impl/client/config/entries/ReloadPluginsEntry.java
@@ -56,7 +56,7 @@ public class ReloadPluginsEntry extends AbstractConfigListEntry<Unit> {
         public void render(PoseStack matrices, int mouseX, int mouseY, float delta) {
             if (PluginManager.areAnyReloading()) {
                 Screen screen = Minecraft.getInstance().screen;
-                Minecraft.getInstance().setScreen(new ConfigReloadingScreen(Component.translatable("text.rei.config.is.reloading"), PluginManager::areAnyReloading, () -> Minecraft.getInstance().setScreen(screen)));
+                Minecraft.getInstance().setScreen(new ConfigReloadingScreen(Component.translatable("text.rei.config.is.reloading"), PluginManager::areAnyReloading, () -> Minecraft.getInstance().setScreen(screen), null));
             } else {
                 super.render(matrices, mouseX, mouseY, delta);
             }
@@ -86,7 +86,7 @@ public class ReloadPluginsEntry extends AbstractConfigListEntry<Unit> {
     
     @Override
     public void save() {
-        
+    
     }
     
     @Override
