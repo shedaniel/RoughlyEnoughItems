6c3f17240d94ed6a14f91b99233e15192e03acb8
Joe Clack <28568841+Lordfirespeed@users.noreply.github.com>
2025-11-29 08:16:09 +0000

    [fix] 1.21.9 NetworkError when viewing tags  (#1979)
    * fix: network protocol error when viewing tags
    feat: re-implement s2c tag packet using codecs
    
    * remove unused methods from `TagData`
    
    * implement `C2STagDataPacket`
    
    * feat: replace manual de/serialization with `C2STagDataPacket`
    
    * style: reformat file
    
    * chore: remove unused import directives
    
    * rename `Uuids` -> `UUIDUtils`
    
    * chore: remove unused `writeResourceLocation`
    
    * chore: remove unused `import` directive
    
    * style: try to restore whitespace to previous state
    
    * chore: `./gradlew licenseFormat`
    
    * style: apply 'keep whitespace on empty lines'
    
    * Prepare for publication
    
    ---------
    
    Co-authored-by: shedaniel <daniel@shedaniel.me>

--- default-plugin/src/main/java/me/shedaniel/rei/plugin/common/displays/tag/TagNodes.java
+++ default-plugin/src/main/java/me/shedaniel/rei/plugin/common/displays/tag/TagNodes.java
@@ -23,18 +23,15 @@
 
 package me.shedaniel.rei.plugin.common.displays.tag;
 
+import com.google.common.collect.Maps;
 import com.mojang.serialization.DataResult;
 import dev.architectury.event.events.client.ClientLifecycleEvent;
-import dev.architectury.impl.NetworkAggregator;
 import dev.architectury.networking.NetworkManager;
-import dev.architectury.networking.transformers.SplitPacketTransformer;
-import dev.architectury.platform.Platform;
 import dev.architectury.utils.Env;
 import dev.architectury.utils.EnvExecutor;
-import io.netty.buffer.Unpooled;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.fastutil.ints.IntList;
-import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
+import me.shedaniel.rei.api.common.util.UUIDUtils;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
@@ -42,13 +39,16 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.TagKey;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -57,8 +57,11 @@ import java.util.function.Consumer;
 
 @ApiStatus.Internal
 public class TagNodes {
-    public static final ResourceLocation REQUEST_TAGS_PACKET_C2S = ResourceLocation.fromNamespaceAndPath("roughlyenoughitems", "request_tags_c2s");
-    public static final ResourceLocation REQUEST_TAGS_PACKET_S2C = ResourceLocation.fromNamespaceAndPath("roughlyenoughitems", "request_tags_s2c");
+    public static final ResourceLocation REQUEST_TAGS_C2S_PACKET_ID = ResourceLocation.fromNamespaceAndPath("roughlyenoughitems", "request_tags_c2s");
+    public static final ResourceLocation REQUEST_TAGS_S2C_PACKET_ID = ResourceLocation.fromNamespaceAndPath("roughlyenoughitems", "request_tags_s2c");
+    
+    public static final CustomPacketPayload.Type<C2STagDataPacket> REQUEST_TAGS_C2S_PACKET_TYPE = new CustomPacketPayload.Type<>(REQUEST_TAGS_C2S_PACKET_ID);
+    public static final CustomPacketPayload.Type<S2CTagDataPacket> REQUEST_TAGS_S2C_PACKET_TYPE = new CustomPacketPayload.Type<>(REQUEST_TAGS_S2C_PACKET_ID);
     
     public static final Map<String, ResourceKey<? extends Registry<?>>> TAG_DIR_MAP = new HashMap<>();
     public static final ThreadLocal<String> CURRENT_TAG_DIR = new ThreadLocal<>();
@@ -88,78 +91,72 @@ public class TagNodes {
     }
     
     public record TagData(IntList otherElements, List<ResourceLocation> otherTags) {
-        private static TagData fromNetwork(FriendlyByteBuf buf) {
-            int count = buf.readVarInt();
-            IntList otherElements = new IntArrayList(count + 1);
-            for (int i = 0; i < count; i++) {
-                otherElements.add(buf.readVarInt());
-            }
-            count = buf.readVarInt();
-            List<ResourceLocation> otherTags = new ArrayList<>(count + 1);
-            for (int i = 0; i < count; i++) {
-                otherTags.add(buf.readResourceLocation());
-            }
-            return new TagData(otherElements, otherTags);
-        }
+        public static final StreamCodec<RegistryFriendlyByteBuf, TagData> STREAM_CODEC = StreamCodec.composite(
+                ByteBufCodecs.collection(IntArrayList::new, ByteBufCodecs.VAR_INT), TagData::otherElements,
+                ByteBufCodecs.collection(ArrayList::new, ResourceLocation.STREAM_CODEC), TagData::otherTags,
+                TagData::new
+        );
+    }
+    
+    public record C2STagDataPacket(UUID uuid, ResourceLocation registryName) implements CustomPacketPayload {
+        public static final StreamCodec<RegistryFriendlyByteBuf, C2STagDataPacket> STREAM_CODEC = StreamCodec.composite(
+                UUIDUtils.STREAM_CODEC, C2STagDataPacket::uuid,
+                ResourceLocation.STREAM_CODEC, C2STagDataPacket::registryName,
+                C2STagDataPacket::new
+        );
         
-        private void toNetwork(FriendlyByteBuf buf) {
-            buf.writeVarInt(otherElements.size());
-            for (int integer : otherElements) {
-                buf.writeVarInt(integer);
-            }
-            buf.writeVarInt(otherTags.size());
-            for (ResourceLocation tag : otherTags) {
-                writeResourceLocation(buf, tag);
-            }
+        @Override
+        public @NotNull Type<? extends CustomPacketPayload> type() {
+            return REQUEST_TAGS_C2S_PACKET_TYPE;
         }
     }
     
-    private static void writeResourceLocation(FriendlyByteBuf buf, ResourceLocation location) {
-        if (location.getNamespace().equals("minecraft")) {
-            buf.writeUtf(location.getPath());
-        } else {
-            buf.writeUtf(location.toString());
+    public record S2CTagDataPacket(UUID uuid, Map<ResourceLocation, TagData> map) implements CustomPacketPayload {
+        public static final StreamCodec<RegistryFriendlyByteBuf, S2CTagDataPacket> STREAM_CODEC = StreamCodec.composite(
+                UUIDUtils.STREAM_CODEC, S2CTagDataPacket::uuid,
+                ByteBufCodecs.map(
+                        Maps::newHashMapWithExpectedSize,
+                        ResourceLocation.STREAM_CODEC,
+                        TagData.STREAM_CODEC
+                ), S2CTagDataPacket::map,
+                S2CTagDataPacket::new
+        );
+        
+        @Override
+        public @NotNull Type<? extends CustomPacketPayload> type() {
+            return REQUEST_TAGS_S2C_PACKET_TYPE;
         }
     }
     
     public static void init() {
         EnvExecutor.runInEnv(Env.CLIENT, () -> Client::init);
-
-        // Fix for TagNodes not being loaded on the server
-        // A bit hacky as it uses Architectury's internal API, but this class needs rewriting to use codecs due to the deprecation of the old serialization system anyway.
-        if(Platform.getEnvironment() != Env.CLIENT) {
-            NetworkAggregator.registerS2CType(REQUEST_TAGS_PACKET_S2C, Collections.singletonList(new SplitPacketTransformer()));
-        }
+        EnvExecutor.runInEnv(Env.SERVER, () -> Server::init);
         
-        NetworkManager.registerReceiver(NetworkManager.c2s(), REQUEST_TAGS_PACKET_C2S, Collections.singletonList(new SplitPacketTransformer()), (buf, context) -> {
-            UUID uuid = buf.readUUID();
-            ResourceKey<? extends Registry<?>> resourceKey = ResourceKey.createRegistryKey(buf.readResourceLocation());
-            RegistryFriendlyByteBuf newBuf = new RegistryFriendlyByteBuf(Unpooled.buffer(), context.registryAccess());
-            newBuf.writeUUID(uuid);
-            Map<ResourceLocation, TagData> dataMap = TAG_DATA_MAP.getOrDefault(resourceKey, Collections.emptyMap());
-            newBuf.writeInt(dataMap.size());
-            for (Map.Entry<ResourceLocation, TagData> entry : dataMap.entrySet()) {
-                writeResourceLocation(newBuf, entry.getKey());
-                entry.getValue().toNetwork(newBuf);
-            }
-            NetworkManager.sendToPlayer((ServerPlayer) context.getPlayer(), REQUEST_TAGS_PACKET_S2C, newBuf);
-        });
+        NetworkManager.registerReceiver(
+                NetworkManager.c2s(),
+                REQUEST_TAGS_C2S_PACKET_TYPE,
+                C2STagDataPacket.STREAM_CODEC,
+                (C2STagDataPacket payload, NetworkManager.PacketContext context) -> {
+                    ResourceKey<? extends Registry<?>> registryKey = ResourceKey.createRegistryKey(payload.registryName);
+                    Map<ResourceLocation, TagData> dataMap = TAG_DATA_MAP.getOrDefault(registryKey, Collections.emptyMap());
+                    var packet = new S2CTagDataPacket(payload.uuid, dataMap);
+                    NetworkManager.sendToPlayer((ServerPlayer) context.getPlayer(), packet);
+                }
+        );
     }
     
     @Environment(EnvType.CLIENT)
     public static void requestTagData(ResourceKey<? extends Registry<?>> resourceKey, Consumer<DataResult<Map<ResourceLocation, TagData>>> callback) {
         if (Minecraft.getInstance().getSingleplayerServer() != null) {
             callback.accept(DataResult.success(TAG_DATA_MAP.get(resourceKey)));
-        } else if (!NetworkManager.canServerReceive(REQUEST_TAGS_PACKET_C2S)) {
+        } else if (!NetworkManager.canServerReceive(REQUEST_TAGS_C2S_PACKET_ID)) {
             callback.accept(DataResult.error(() -> "Cannot request tags from server"));
         } else if (requestedTags.containsKey(resourceKey)) {
             requestedTags.get(resourceKey).accept(callback);
             callback.accept(DataResult.success(TAG_DATA_MAP.getOrDefault(resourceKey, Collections.emptyMap())));
         } else {
-            RegistryFriendlyByteBuf buf = new RegistryFriendlyByteBuf(Unpooled.buffer(), BasicDisplay.registryAccess());
             UUID uuid = UUID.randomUUID();
-            buf.writeUUID(uuid);
-            buf.writeResourceLocation(resourceKey.location());
+            var packet = new C2STagDataPacket(uuid, resourceKey.location());
             Client.nextUUID = uuid;
             Client.nextResourceKey = resourceKey;
             List<Consumer<DataResult<Map<ResourceLocation, TagData>>>> callbacks = new CopyOnWriteArrayList<>();
@@ -171,7 +168,13 @@ public class TagNodes {
                 }
             };
             requestedTags.put(resourceKey, callbacks::add);
-            NetworkManager.sendToServer(REQUEST_TAGS_PACKET_C2S, buf);
+            NetworkManager.sendToServer(packet);
+        }
+    }
+    
+    private static class Server {
+        private static void init() {
+            NetworkManager.registerS2CPayloadType(REQUEST_TAGS_S2C_PACKET_TYPE, S2CTagDataPacket.STREAM_CODEC);
         }
     }
     
@@ -184,23 +187,22 @@ public class TagNodes {
             ClientLifecycleEvent.CLIENT_LEVEL_LOAD.register(world -> {
                 requestedTags.clear();
             });
-            NetworkManager.registerReceiver(NetworkManager.s2c(), REQUEST_TAGS_PACKET_S2C, (buf, context) -> {
-                UUID uuid = buf.readUUID();
-                if (nextUUID.equals(uuid)) {
-                    Map<ResourceLocation, TagData> map = new HashMap<>();
-                    int count = buf.readInt();
-                    for (int i = 0; i < count; i++) {
-                        map.put(buf.readResourceLocation(), TagData.fromNetwork(buf));
+            
+            NetworkManager.registerReceiver(
+                    NetworkManager.s2c(),
+                    REQUEST_TAGS_S2C_PACKET_TYPE,
+                    S2CTagDataPacket.STREAM_CODEC,
+                    (S2CTagDataPacket payload, NetworkManager.PacketContext context) -> {
+                        if (!nextUUID.equals(payload.uuid)) return;
+                        
+                        TAG_DATA_MAP.put(nextResourceKey, payload.map);
+                        nextCallback.accept(DataResult.success(payload.map));
+                        
+                        nextUUID = null;
+                        nextResourceKey = null;
+                        nextCallback = null;
                     }
-                    
-                    TAG_DATA_MAP.put(nextResourceKey, map);
-                    nextCallback.accept(DataResult.success(map));
-                    
-                    nextUUID = null;
-                    nextResourceKey = null;
-                    nextCallback = null;
-                }
-            });
+            );
         }
     }
     
@@ -232,7 +234,8 @@ public class TagNodes {
                 Optional<DataResult<TagNode<T>>> resultOptional = resolveTag(childTagKey, registry, tagDataMap);
                 if (resultOptional.isPresent()) {
                     DataResult<TagNode<T>> result = resultOptional.get();
-                    if (result.error().isPresent()) return Optional.of(DataResult.error(() -> result.error().get().message()));
+                    if (result.error().isPresent())
+                        return Optional.of(DataResult.error(() -> result.error().get().message()));
                     self.addChild(result.result().get());
                 }
             }
