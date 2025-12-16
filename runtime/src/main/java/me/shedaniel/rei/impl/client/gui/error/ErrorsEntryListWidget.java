c5d4d6dd7b00fd6eb148c75f32fc19bd9cb839e0
michiel1106 <michielimmink@gmail.com>
2025-12-16 18:41:38 +0100

    Progress

--- runtime/src/main/java/me/shedaniel/rei/impl/client/gui/error/ErrorsEntryListWidget.java
+++ runtime/src/main/java/me/shedaniel/rei/impl/client/gui/error/ErrorsEntryListWidget.java
@@ -28,9 +28,9 @@ import me.shedaniel.clothconfig2.gui.widget.DynamicEntryListWidget;
 import me.shedaniel.clothconfig2.gui.widget.DynamicSmoothScrollingEntryListWidget;
 import me.shedaniel.rei.impl.client.gui.InternalTextures;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.*;
+import net.minecraft.client.gui.*;
 import net.minecraft.util.*;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.events.ContainerEventHandler;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.narration.NarratableEntry;
@@ -46,6 +46,7 @@ import net.minecraft.resources.Identifier;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.util.Mth;
+import org.apache.commons.lang3.mutable.*;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix3x2f;
@@ -353,13 +354,32 @@ public class ErrorsEntryListWidget extends DynamicSmoothScrollingEntryListWidget
                     int line = textY / 12;
                     if (line < this.textSplit.size()) {
                         FormattedCharSequence orderedText = this.textSplit.get(line);
-                        return Minecraft.getInstance().font.getSplitter().componentStyleAtWidth(orderedText, textX);
+                        return styleAtWidth(orderedText, textX, Minecraft.getInstance().font);
                     }
                 }
             }
             
             return null;
         }
+    }
+
+    @Nullable
+    private static Style styleAtWidth(FormattedCharSequence text, int width, Font font) {
+        StringSplitter splitter = font.getSplitter();
+        StringSplitter.WidthLimitedCharSink sink =
+                splitter.new WidthLimitedCharSink(width);
+
+        final MutableObject<Style> result = new MutableObject<>();
+
+        text.accept((i, style, codepoint) -> {
+            if (!sink.accept(i, style, codepoint)) {
+                result.setValue(style);
+                return false;
+            }
+            return true;
+        });
+
+        return result.getValue();
     }
     
     public static class HorizontalRuleEntry extends Entry {
